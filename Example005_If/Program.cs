8a28b3e9585e0dc8ab1316f814efd8c655aa7d92
Roman Korneev <roman.korneev@inbox.ru>
2022-01-31 15:51:50 +0300

    Add example 005 If

---
+++ Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, its a Masha!");
+}
+else
+{
+    Console.WriteLine("Hi, " + username);
+}
